b6aba8ec41d3e38c6e488c835845ff0a28e6e57e
Peter Yang <peter000811023@gmail.com>
2024-08-09 16:54:08 +0800

    update desc

--- src/Figma_HideLoginPopupWindow.js
+++ src/Figma_HideLoginPopupWindow.js
@@ -1,12 +1,14 @@
 // ==UserScript==
 // @name         Figma: 沒登入時，隱藏登入蓋版框
-// @namespace    http://tampermonkey.net/
+// @namespace    https://raw.githubusercontent.com/PeterYHH/TampermonkeyScript/main/src/Figma_HideLoginPopupWindow.js
 // @version      20240808001
 // @description  imga: 沒登入時，隱藏登入蓋版框
 // @author       PeterYang
 // @match        https://www.figma.com/design/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=figma.com
 // @grant        none
+// @source       https://raw.githubusercontent.com/PeterYHH/TampermonkeyScript/main/src/Figma_HideLoginPopupWindow.js
+
 // ==/UserScript==
 
 (function() {
